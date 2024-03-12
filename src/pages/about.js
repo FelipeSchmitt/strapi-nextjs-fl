71f32e832174cb336d9f88574e5d5b1c693f8d45
Felipe Schmitt <50593950+FelipeSchmitt@users.noreply.github.com>
2024-03-12 15:10:05 -0300

    Add files via upload

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import Image from 'next/image';
+
+const About = () => {
+    return(
+        <div>
+            <Image src="/topo.webp" alt="Imagem Topo" layout='fill' objectFit='contain' />
+            <Image src="/topo.webp" alt="Imagem Topo" layout='fill' objectFit='contain' />
+            <Image src="/topo.webp" alt="Imagem Topo" layout='fill' objectFit='contain' />
+        </div>
+    )
+}
+
+export default About;
